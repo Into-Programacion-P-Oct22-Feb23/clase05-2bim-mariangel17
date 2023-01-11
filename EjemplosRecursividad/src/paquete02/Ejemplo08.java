30d1fbc93a9c16baa1c9091c1574485c6fd91704
Mariangel Rojas <mariangelrojasrojas7@gmail.com>
2023-01-11 10:40:57 -0500

    commit

--- EjemplosRecursividad/src/paquete02/Ejemplo08.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo08.java
@@ -4,12 +4,29 @@
  */
 package paquete02;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ejemplo08 {
+    
+   
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
         // Ingresar por teclado el tamaño de un arreglo
         // Ingresar por teclado los valores enteros del arreglo
         // Sumar los valores del arreglo, mediante la función misterio
+        int tamaño;
+       
+        System.out.println("Ingrese el tamaño del arreglo");
+        tamaño = entrada.nextInt();
+        int [] arreglo = new int [tamaño];
+        for (int i = 0; i < tamaño; i++) {
+            System.out.println("Ingrese los valores del arreglo"); 
+            arreglo[i]= entrada.nextInt();
+        }
+        
+        System.out.printf("El valor es: %s\n",misterio(arreglo,tamaño));
         
     }
 
